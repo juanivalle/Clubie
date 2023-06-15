61b48a71c3767137cea6813b78b23795a5cb5461
juanivalle <juanignaciovalle18@gmail.com>
2023-06-14 23:36:56 -0300

    home casi terminado

--- css/js/home.js
+++ css/js/home.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById("myVideo");
-    var logoMovil = document.getElementById("logo-movil");
-    let sec = document.querySelectorAll('section');
-    let links = document.querySelectorAll('nav a');
+  // Obtener todos los enlaces de la navbar
+  const navbarLinks = document.querySelectorAll("nav ul li a");
 
-    video.addEventListener("click", function() {
-      if (video.paused) {
-        video.play();
-      } else {
-        video.pause();
-      }
-    })});
+  // Agregar el evento de clic a cada enlace
+  navbarLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+
+      // Obtener el identificador de la sección objetivo del enlace
+      const target = link.getAttribute("href");
+
+      // Desplazarse suavemente a la sección objetivo
+      document.querySelector(target).scrollIntoView({
+      });
+    });
+  });
+});
