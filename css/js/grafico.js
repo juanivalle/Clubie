53dba05d8c11a62fb2a45732dca038b51fc0b538
juanivalle <juanignaciovalle18@gmail.com>
2023-06-30 12:40:14 -0300

    hice 2 graficos mas, necesito las bases de datos y ayuda de back para algunas cosas, despues ya arregle las boludeces de las paginas login

--- css/js/grafico.js
+++ css/js/grafico.js
@@ -28,6 +28,79 @@ fetch('/prueba')
       }
     });
   })
+  
   .catch(error => {
     console.error('Error:', error);
   });
+
+  fetch('/prueba')
+  .then(response => response.json())
+  .then(data => {
+    // Preparar los datos para el gráfico
+    const labels = data.map(user => user.name);
+    const values = data.map(user => user.telefono);
+    enableEventHandlers(labels);
+
+    // Configurar y generar el gráfico utilizando Chart.js
+    const ctx = document.getElementById('featuresChart').getContext('2d');
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Teléfonos',
+          data: values,
+          backgroundColor: getDataColors(),
+          borderColor: getDataColors(),
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  })
+  
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+  fetch('/prueba')
+  .then(response => response.json())
+  .then(data => {
+    // Preparar los datos para el gráfico
+    const labels = data.map(user => user.name);
+    const values = data.map(user => user.telefono);
+
+    // Configurar y generar el gráfico utilizando Chart.js
+    const ctx = document.getElementById('yearsChart').getContext('2d');
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Teléfonos',
+          data: values,
+          backgroundColor: getDataColors(),
+          borderColor: getDataColors(),
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  })
+  
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
--- css/js/handlers.js
+++ css/js/handlers.js
@@ -1,4 +1,4 @@
-const enableEventHandlers = coasters => {
+const enableEventHandlers = (coasters) => {
 
     document.querySelector('#featuresOptions').onchange = e => {
 
