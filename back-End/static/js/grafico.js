432d0c283bff08c3464ca76d9539584ae607f107
juanivalle <juanignaciovalle18@gmail.com>
2023-07-05 11:59:14 -0300

    hablar con nacho para que ayude

--- back-End/static/js/grafico.js
+++ back-End/static/js/grafico.js
@@ -75,12 +75,14 @@ fetch('/prueba')
     // Preparar los datos para el gráfico
     const labels = data.map(venta => venta.retiro);
     const values = data.map(venta => venta.cantidad);
+    
     // Configurar y generar el gráfico utilizando Chart.js
+    const month = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Sepiembre', 'Octubre', 'Noviembre', 'Diciembre'];
     const ctx = document.getElementById('yearsChart').getContext('2d');
     new Chart(ctx, {
       type: 'line',
       data: {
-        labels: labels,
+        labels: month,
         datasets: [{
           label: 'retiro',
           data: values,
--- back-End/static/js/helpers.js
+++ back-End/static/js/helpers.js
@@ -1,19 +1,16 @@
-const fetchCoastersData = (...urls) => {
-    const promises = urls.map(url => fetch(url).then(response => response.json()))
-    return Promise.all(promises)
-}
+
 
 const getDataColors = opacity => {
     const colors = ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(255, 206, 86, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(153, 102, 255, 0.5)', 'rgba(255, 159, 64, 0.5)', 'rgba(136, 255, 156, 0.5)', 'rgba(235, 166, 54, 0.5)', 'rgba(132, 209, 235, 0.5)', 'rgba(255, 112, 197, 0.5)']
     return colors.map(color => opacity ? `${color + opacity}` : color)
 }
 
-const getCoastersByYear = (coasters, years) => {
-    const coastersByYear = years.map(yearsRange => {
-        const [from, to] = yearsRange.split('-')
-        return coasters.filter(eachCoaster => eachCoaster.year >= from && eachCoaster.year <= to).length
+const meses = (retiro, month) => {
+    const mesesVentas = month.map(monthRange => {
+        const [from, to] = monthRange.split('-')
+        return coasters.filter(retiro => retiro.month >= from && retiro.month <= to).length
     })
-    return coastersByYear
+    return mesesVentas
 }
 
 const updateChartData = (chartId, data, label) => {
