bb992e8232412d03cb0ac160125d6b410c3221bb
juanivalle <juanignaciovalle18@gmail.com>
2023-07-05 16:31:59 -0300

    estamos muy pero muy despegados

--- back-End/static/js/grafico.js
+++ back-End/static/js/grafico.js
@@ -82,19 +82,26 @@ fetch('/prueba')
   .then(response => response.json())
   .then(data => {
     // Preparar los datos para el gráfico
-    const labels = data.map(venta => venta.retiro);
-    const values = data.map(venta => venta.cantidad);
+    const months = Array(12).fill(0);
+    const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
     
+    // Agrupar la cantidad retirada por mes
+    data.forEach(venta => {
+      const date = new Date(venta.retiro);
+      const month = date.getMonth();
+      const cantidad = venta.cantidad;
+      months[month] += cantidad;
+    });
+
     // Configurar y generar el gráfico utilizando Chart.js
-    const month = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Sepiembre', 'Octubre', 'Noviembre', 'Diciembre'];
     const ctx = document.getElementById('yearsChart').getContext('2d');
     new Chart(ctx, {
       type: 'line',
       data: {
-        labels: month,
+        labels: labels,
         datasets: [{
-          label: 'retiro',
-          data: values,
+          label: 'Cantidad Retirada',
+          data: months,
           backgroundColor: getDataColors(),
           borderColor: getDataColors(),
           borderWidth: 1
@@ -109,8 +116,6 @@ fetch('/prueba')
       }
     });
   })
-  
   .catch(error => {
     console.error('Error:', error);
   });
-
