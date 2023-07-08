ff1773fcb79c1b3c4d81b27ebedfdb72e3c7b383
Agus <agustinmeriles@hotmail.com>
2023-07-08 15:44:53 -0300

    .

--- back-End/app.py
+++ back-End/app.py
@@ -136,7 +136,7 @@ def delete_planta(idRaza):
     if elimplanta:
         db.session.delete(elimplanta)
         db.session.commit()
-  
+
     return redirect(url_for('homeplantcreo'))
 
 ##########################################################################################################################################
@@ -238,7 +238,7 @@ def obDatosU():
 #     return render_template('/noLog/contact.html')
 
 
-@app.route('/login.html')
+@app.route('/login.html', methods=['POST', 'GET'])
 def login():
     form = LoginForm
     return render_template('/noLog/login.html', form=form)
